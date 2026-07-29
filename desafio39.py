18d8d349387b5055e2a8f3a3a87a5e77943f0a75
Jonas-Miguel <jonasmiguel1520@gmail.com>
2026-07-29 11:37:07 -0300

    desafio39

--- desafio39.py
+++ desafio39.py
@@ -1,6 +1,16 @@
-nome = input('Qual o seu nome?')
-data = input('olá {},qual a seu ano de nascimento? '.format(nome))
-ano_atual = int(input('Qual o ano atual? '))
-ano_alistamento = data + 18
-if ano_atual >= ano_alistamento:
-    
+from datetime import date
+atual = date.today().year
+nasc = int(input('Digite o ano de nascimento: '))
+idade = atual - nasc
+if idade == 18:
+    print('Você deve se alistar imediatamente! ')
+elif idade < 18:
+    saldo = 18 - idade
+    print('Ainda faltam {} anos para o alistamento'.format(saldo))
+    ano = atual + saldo
+    print('Seu alistamento será em {}'.format(ano))
+elif idade > 18:
+    saldo = idade - 18
+    print('Você deveria ter se alisatado em {} anos'.format(saldo))
+    ano = atual - saldo
+    print('Seu alistamento foi em {}'.format(ano))
