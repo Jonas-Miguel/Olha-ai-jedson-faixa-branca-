ed5b2eac5db87dc551bd81d62a2af9ec5ba44037
Jonas-Miguel <jonasmiguel1520@gmail.com>
2026-07-30 19:28:17 -0300

    triangulo

--- desafio35.py
+++ desafio35.py
@@ -1,7 +1,9 @@
 r1 = float(input('Digite cumprimento da primeira reta: '))
 r2 = float(input('Digite o cumprimento da segunda reta: '))
 r3 = float(input('Digite o cumprimento da terceira reta: '))
-if (r1 + r2) > r3 and (r1 + r3) > r2 and (r2 + r3) > r1:
-    print('As retas informadas PODEM formar um triangulo')
+if (r1 == r2) and (r1 == r3)  and (r2 == r3):
+    print('As retas informadas formam um triangulo, EQUILATERO')
+elif (r1 == r2 != r3) and (r1 == r3 != r2)  and (r2 == r3 != r1 ):
+    print('As retas indicadas formam um triangulo ISÓCELES')
 else:
-    print('As retas informadas NÃO PODEM formar um triangulo')
+    print('As retas indicadas forman um triangulo ESCALENO')
